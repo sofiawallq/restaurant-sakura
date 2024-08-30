7c12c7d076689bc89e9a8eb2c475f8036b12614f
Sofia Wallqvist <sofiawallqvist@gmail.com>
2024-08-30 11:41:39 +0000

    create url paths for viewing and editing reservations

--- booking/urls.py
+++ booking/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('booking/', views.book_table, name='booking'),
-    
+    path('booking/edit/<int:booking_id>/', views.booking_edit, name='booking_edit'),
+    path('booking/new/', views.booking_edit, name='booking_new'),
 ]
