d5406f45b6ae2d62da2ded12185a67273d233562
Sofia Wallqvist <sofiawallqvist@gmail.com>
2024-09-03 17:16:45 +0000

    update timepicker customized for opening hours

--- booking/forms.py
+++ booking/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import BookATable
+import datetime
 
 class BookATableForm(forms.ModelForm):
     class Meta:
@@ -7,7 +8,14 @@ class BookATableForm(forms.ModelForm):
         fields = ['firstname', 'lastname', 'email', 'guests', 'date', 'time', 'message']         
         widgets = {
             'date': forms.DateInput(attrs={'class': 'form-control', 'id': 'date_picker'}),
-            'time': forms.TimeInput(attrs={'class': 'form-control', 'id': 'time_picker'}),
+            # 'time': forms.TimeInput(attrs={'class': 'form-control', 'id': 'time_picker'}),
         }
+
+
+    time_choices = [(datetime.time(hour, minute), f'{hour:02d}:{minute:02d}')
+                    for hour in range(15, 23)
+                    for minute in (0, 30)]
+
+    time = forms.ChoiceField(choices=time_choices, widget=forms.Select(attrs={'class': 'form-control', 'id': 'time_picker'}))        
 
              
