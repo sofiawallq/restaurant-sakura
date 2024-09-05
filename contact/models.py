838e7e117a8342e5e660d28d94017609bcf124a0
Sofia Wallqvist <sofiawallqvist@gmail.com>
2024-09-05 20:19:48 +0000

    create docstring for contact models.py

--- contact/models.py
+++ contact/models.py
@@ -3,6 +3,18 @@ from cloudinary.models import CloudinaryField
 
 
 class ContactRestaurant(models.Model):
+    """
+    Model to store customer inquiries/messages sent to the restaurant.
+    Holds details such as subject, sender's name, email, and message content. 
+    It also includes a 'read' field to track whether the message 
+    has been marked as read by the site admin in the admin panel.
+    Contains fields for:
+        subject (str) - subject of the inquiry.
+        name (str) - name of the person sending the message.
+        email (EmailField) - email address of the sender.
+        message (TextField) - body of the message or inquiry.
+        read (bool) - Indicates if the message has been marked as read.
+    """
     subject = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
     email = models.EmailField()
